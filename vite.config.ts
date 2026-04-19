7751c7f4d9b0559b9aa24debaa401250cdbf1785
tchw521 <59728979+tchw521@users.noreply.github.com>
2026-04-19 21:23:34 +0800

    fix: 消除所有 Sass 弃用警告

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/variables.scss";`
+        silenceDeprecations: ['legacy-js-api', 'import']
       }
     }
   }
